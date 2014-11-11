7fb6ffe4769556966eea76fa3303da310bec09c4
Limbach <limbach@localhost.localdomain>
2014-11-11 19:06:27 +0100

    Initialer Commit im neuen Repository

---
+++ src/main/java/de/yellow/medienverwaltung/controller/MyController.java
@@ -0,0 +1,32 @@
+package de.yellow.medienverwaltung.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+    String message = "Welcome to Spring MVC!";
+
+    
+    //Unter "medienverwaltung/" geben wir die index.jsp zurück
+    @RequestMapping("/")
+    public ModelAndView showIndex() {
+        System.out.println("in controller");
+
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
+    //und unter "medienverwaltung/hello die helloworld.jsp
+    @RequestMapping("/hello")
+    public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        System.out.println("in controller");
+
+        ModelAndView mv = new ModelAndView("helloworld");
+        mv.addObject("message", message);
+        mv.addObject("name", name);
+        return mv;
+    }
+}
