e04628dbc5ba993d18c56d8d4fb2c94040e86d92
Krispin Limbach <limbach@buch.de>
2014-11-12 16:05:47 +0100

    Fix .gitignore

--- src/main/java/de/yellow/medienverwaltung/controller/MyController.java
+++ src/main/java/de/yellow/medienverwaltung/controller/MyController.java
@@ -9,20 +9,19 @@ import org.springframework.web.servlet.ModelAndView;
 public class MyController {
     String message = "Welcome to Spring MVC!";
 
-    
-    //Unter "medienverwaltung/" geben wir die index.jsp zurück
+    // Unter "medienverwaltung/" geben wir die index.jsp zurück
     @RequestMapping("/")
     public ModelAndView showIndex() {
-        System.out.println("in controller");
+        System.out.println("index");
 
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
 
-    //und unter "medienverwaltung/hello die helloworld.jsp
+    // und unter "medienverwaltung/hello die helloworld.jsp
     @RequestMapping("/hello")
     public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-        System.out.println("in controller");
+        System.out.println("/hello");
 
         ModelAndView mv = new ModelAndView("helloworld");
         mv.addObject("message", message);
