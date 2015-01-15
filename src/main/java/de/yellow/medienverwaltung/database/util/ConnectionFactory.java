8eb04553c26c9c4bd9ebec7e9f949699ee5ffdd5
Krispin Limbach <limbach@buch.de>
2015-01-15 10:57:29 +0100

    Datenbankverbindung in .properties ausgelagert + DataTable aktualisiert
    Zur leichteren Konfiguration der Anwendung wurden die Verbindungsparameter
    zur Datenbank in eine .properties - Datei ausgelagert.
    
    Die jQuery DataTable wurde auf die neueste Version aktulisiert.

--- src/main/java/de/yellow/medienverwaltung/database/util/ConnectionFactory.java
+++ src/main/java/de/yellow/medienverwaltung/database/util/ConnectionFactory.java
@@ -1,38 +1,55 @@
 package de.yellow.medienverwaltung.database.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
- * Factory-Klasse zum erzeugen einer {@link DataSource}. Mit der
- * {@link DataSource} werden {@link JdbcTemplate}s erzeugt.
+ * Factory-Klasse zum erzeugen einer {@link DataSource}. Mit der {@link DataSource} werden {@link JdbcTemplate}s erzeugt.
  * 
  * @author krispin
- *
+ * 
  */
 public class ConnectionFactory {
 
-	// TODO: In properties auslagern!
-	private String host = "localhost";
-	private String db = "media";
-	private String port = "3306";
-	private String user = "root";
-	private String pw = "";
+    private String host;
+    private String db;
+    private String port;
+    private String user;
+    private String pw;
 
-	public DataSource getDataSource() {
+    public DataSource getDataSource() {
 
-		// TODO: Logger statt System.out
-		System.out
-				.println("Aus den Parametern ein DataSource für die Datenbank erstellen");
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
 
-		String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
 
-		DriverManagerDataSource dataSource = new DriverManagerDataSource(url,
-				user, pw);
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		return dataSource;
-	}
+        try {
+            prop.load(inputStream);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Konnte Properties nicht laden");
+        }
+
+        host = prop.getProperty("host");
+        db = prop.getProperty("db");
+        port = prop.getProperty("port");
+        user = prop.getProperty("user");
+        pw = prop.getProperty("pw");
+
+        // TODO: Logger statt System.out
+        System.out.println("Aus den Parametern ein DataSource für die Datenbank erstellen");
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(url, user, pw);
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
 
 }
